80854e9ffce6267aa9e4cd42ea07812a6d0e6c5b
ML-std <ML-std@github. com>
2022-07-26 12:46:43 +0300

    Initial commit

---
+++ src/main/java/springbootunittesting/fizzbuzzproject/FizzbuzzProjectApplication.java
@@ -0,0 +1,13 @@
+package springbootunittesting.fizzbuzzproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FizzbuzzProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springbootunittesting/fizzbuzzproject/FizzbuzzProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package springbootunittesting.fizzbuzzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+}
